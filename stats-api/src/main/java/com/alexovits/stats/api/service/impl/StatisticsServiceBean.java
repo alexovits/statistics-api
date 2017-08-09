3643489239a0a6d105676dcb3cbb1be3a5e9d8a7
alexovits <debeloksanyi@gmail.com>
2017-08-09 20:21:42 +0300

    Add code documentation to the StatisticsServiceBean

--- stats-api/src/main/java/com/alexovits/stats/api/service/impl/StatisticsServiceBean.java
+++ stats-api/src/main/java/com/alexovits/stats/api/service/impl/StatisticsServiceBean.java
@@ -38,8 +38,12 @@ public class StatisticsServiceBean implements StatisticsService{
 
     }
 
+    /** Initializes attributes of the class and starts a daemon thread that will repeat every second **/
     @PostConstruct
     public void init(){
+        transactionSum = 0;
+        maxTransaction = Integer.MIN_VALUE;
+        minTransaction = Integer.MAX_VALUE;
         latestTransactions = new ArrayList<>();
         ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
         // Using scheduleWithFixedDelay because it counts delay from the end of the thread
@@ -51,8 +55,15 @@ public class StatisticsServiceBean implements StatisticsService{
         );
     }
 
+    /**
+     * Inserts a new transaction into the collection.
+     * If the transaction is already expired InvalidTransactionException gets thrown.
+     * @param transaction Transaction object containing a new transaction from a client
+     * @throws InvalidTransactionException
+     */
     @Override
     public void insertTransaction(Transaction transaction) throws InvalidTransactionException {
+        // Read lock needed because this method should be blocked whenever getStats or analyzeTransactions is being executed.
         lock.readLock().lock();
         try {
             if(checkExpiredTimestamp(transaction.getTimestamp())) {
@@ -65,6 +76,10 @@ public class StatisticsServiceBean implements StatisticsService{
         }
     }
 
+    /**
+     * Creates a Statistics object from the current data
+     * @return Statistics with the current informations
+     */
     @Override
     public Statistics getStats() {
         // Write lock needed since the daemon thread could change the values while constructing the response
@@ -80,7 +95,14 @@ public class StatisticsServiceBean implements StatisticsService{
         }
     }
 
+    /**
+     * This method gets called every second.
+     * As a daemon thread this methods filters out all the old transactions and updates the current statistics.
+     * The reason for the statistics being recalculated is because accessing these information should be O(1)
+     * and if the getStats() would have to iterate the collection every time it would make it O(n).
+     */
     private void analyzeTransactions() {
+        // Write lock needed since the insertion method's would interfere with the calculations. They are put on hold for the time being.
         lock.writeLock().lock();
         try {
             // Filter out the transactions that are older than one second
@@ -102,12 +124,16 @@ public class StatisticsServiceBean implements StatisticsService{
                     minTransaction = tr.getAmount();
                 }
             });
-
         } finally {
             lock.writeLock().unlock();
         }
     }
 
+    /**
+     * Checks whether a timestamp is older tha 1 minute
+     * @param timestamp the timestamp to check the condition against
+     * @return true if the timestamp is expired, false otherwise
+     */
     private boolean checkExpiredTimestamp(Instant timestamp) {
         return Instant.now().minusSeconds(60).isAfter(timestamp) ? true : false;
     }
